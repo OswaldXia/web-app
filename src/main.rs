58e42682557216d9512cd9a30834b33d2f536800
Oswald <max01-xwn@live.cn>
2022-11-15 01:08:54 +0800

    return an unauthorized response if the authorization fails instead of passing our request to the next layer

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ mod state;
 mod to_do;
 mod views;
 
-use actix_web::dev::Service;
-use actix_web::{App, HttpServer};
+use actix_web::{
+    dev::Service,
+    App, HttpResponse, HttpServer,
+};
+
+use futures::future::{ok, Either};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -17,18 +21,20 @@ async fn main() -> std::io::Result<()> {
         let app = App::new()
             // pass in the service request and the routing
             .wrap_fn(|req, srv| {
-                if req.path().contains("/item/") {
-                    match auth::process_token(&req) {
-                        Ok(_) => println!("the token is passable"),
-                        Err(msg) => println!("token error: {}", msg),
-                    }
+                // flag used to indicate whether the authentication is passed
+                let passed: bool;
+                if req.path().contains("/item/") 
+                && auth::process_token(&req).is_err() {
+                    passed = false;
+                } else {
+                    // not checking credentials for other calls
+                    passed = true;
                 }
-                // create a future with the call function belonging to the routing
-                let fut = srv.call(req);
-                async {
-                    // await for this to complete and return the result
-                    let result = fut.await?;
-                    Ok(result)
+
+                if passed {
+                    Either::Left(srv.call(req))
+                } else {
+                    Either::Right(ok(req.into_response(HttpResponse::Unauthorized().finish())))
                 }
             })
             .configure(views::views_factory);
